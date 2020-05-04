55e9c7310f1c8e09349576c948ed4c17627f86b7
chiehjerry <chiehjerry@gmail.com>
2020-05-04 10:27:40 +0800

    to_SKYHOMEMADE_logo_main

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
 
 $(function () {
   $('#main_').click(function () {
-    $('html,body').animate({ scrollTop: $('#main_').offset().top }, "show");
+    $('html,body').animate({ scrollTop: $('#main_').offset().top }, 1000);
+    return false;
+  });
+});
+$(function () {
+  $('#service_').click(function () {
+    $('html,body').animate({ scrollTop: $('#service').offset().top }, 1000);
     return false;
   });
 });
 $(function () {
   $('#latest_').click(function () {
-    $('html,body').animate({ scrollTop: $('#latest').offset().top }, "show");
-    return false;
-  });
-});
-$(function () {
-  $('#email_').click(function () {
-    $('html,body').animate({ scrollTop: $('#email').offset().top }, "show");
+    $('html,body').animate({ scrollTop: $('#latest').offset().top }, 1000);
     return false;
   });
 });
 $(function () {
   $('#connect_').click(function () {
-    $('html,body').animate({ scrollTop: $('#connect').offset().top }, "show");
+    $('html,body').animate({ scrollTop: $('#connect').offset().top }, 1000);
     return false;
   });
 });
+
+$(function () {
+  $('#main__').click(function () {
+    $('html,body').animate({ scrollTop: $('#main_').offset().top }, 1000);
+    return false;
+  });
+});
+$(function () {
+  $('#service__').click(function () {
+    $('html,body').animate({ scrollTop: $('#service').offset().top }, 1000);
+    return false;
+  });
+});
+$(function () {
+  $('#latest__').click(function () {
+    $('html,body').animate({ scrollTop: $('#latest').offset().top }, 1000);
+    return false;
+  });
+});
+
